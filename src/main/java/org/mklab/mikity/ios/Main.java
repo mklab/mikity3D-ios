05b3a4304e4fea13a15faedbfe83fef24447da40
Eguchi Toru <eguchi@Eguchi-no-MacBook-Air.local>
2016-01-06 20:05:22 +0900

    add navigation controller (test)

--- src/main/java/org/mklab/mikity/ios/Main.java
+++ src/main/java/org/mklab/mikity/ios/Main.java
@@ -13,37 +13,33 @@ import org.robovm.apple.uikit.UIApplication;
 import org.robovm.apple.uikit.UIApplicationDelegateAdapter;
 import org.robovm.apple.uikit.UIApplicationLaunchOptions;
 import org.robovm.apple.uikit.UIColor;
+import org.robovm.apple.uikit.UINavigationController;
 import org.robovm.apple.uikit.UIScreen;
 import org.robovm.apple.uikit.UIWindow;
 
-public class Main extends UIApplicationDelegateAdapter implements GLKViewDelegate, GLKViewControllerDelegate {
+public class Main extends UIApplicationDelegateAdapter implements GLKViewControllerDelegate {
 	private UIWindow window = null;
 	private boolean increasing = false;
 	private float curRed = 0.0f;
-	private GLRendererSample sampleView;
 
 	@Override
 	public boolean didFinishLaunching(UIApplication application, UIApplicationLaunchOptions launchOptions) {
 		this.window = new UIWindow(UIScreen.getMainScreen().getBounds());
 		EAGLContext context = new EAGLContext(EAGLRenderingAPI.OpenGLES1);
 		GLKView view = new GLKView(UIScreen.getMainScreen().getBounds(), context);
-		sampleView = new GLRendererSample();
-		sampleView.onSurfaceCreated(null);
-		CGRect bounds = UIScreen.getMainScreen().getBounds();
-		sampleView.onSurfaceChanged(null, (int)bounds.getWidth(), (int)bounds.getHeight());
-		view.setDelegate(this);
-//		this.window.addSubview(view);
-
-		GLKViewController viewController = new GLKViewController();
-		viewController.setView(view);
 		
-//		GLRendererSample viewController = new GLRendererSample();
-//		viewController.setView(view);
+		GLRendererSample viewController = new GLRendererSample();
+		viewController.viewDidLoad();
+		view.setDelegate(viewController);
+		
+		UINavigationController navi = new UINavigationController(viewController);
+
+		viewController.setView(view);
 		
 //		final MyGLKViewControllerDelegate gcDelegate = new MyGLKViewControllerDelegate(this);
 		viewController.setDelegate(this);
 		viewController.setPreferredFramesPerSecond(60);
-		this.window.setRootViewController(viewController);
+		this.window.setRootViewController(navi);
 		
 		this.window.setBackgroundColor(UIColor.white());
 		this.window.makeKeyAndVisible();
