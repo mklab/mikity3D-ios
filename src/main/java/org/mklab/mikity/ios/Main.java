9c5713508f007640a2309ac9b1592def71974103
Eguchi Toru <eguchi@Eguchi-no-MacBook-Air.local>
2016-01-12 21:05:40 +0900

    set Navigation code

--- src/main/java/org/mklab/mikity/ios/Main.java
+++ src/main/java/org/mklab/mikity/ios/Main.java
@@ -2,6 +2,7 @@ package org.mklab.mikity.ios;
 
 import org.mklab.mikity.ios.sample.GLRendererSample;
 import org.robovm.apple.coregraphics.CGRect;
+import org.robovm.apple.coregraphics.CGSize;
 import org.robovm.apple.foundation.NSArray;
 import org.robovm.apple.foundation.NSAutoreleasePool;
 import org.robovm.apple.glkit.GLKView;
@@ -13,8 +14,17 @@ import org.robovm.apple.opengles.EAGLRenderingAPI;
 import org.robovm.apple.uikit.UIApplication;
 import org.robovm.apple.uikit.UIApplicationDelegateAdapter;
 import org.robovm.apple.uikit.UIApplicationLaunchOptions;
+import org.robovm.apple.uikit.UIButton;
+import org.robovm.apple.uikit.UIButtonType;
 import org.robovm.apple.uikit.UIColor;
+import org.robovm.apple.uikit.UIControl;
+import org.robovm.apple.uikit.UIControlState;
+import org.robovm.apple.uikit.UIEvent;
+import org.robovm.apple.uikit.UIFont;
+import org.robovm.apple.uikit.UIModalPresentationStyle;
 import org.robovm.apple.uikit.UINavigationController;
+import org.robovm.apple.uikit.UIPopoverArrowDirection;
+import org.robovm.apple.uikit.UIPopoverPresentationController;
 import org.robovm.apple.uikit.UIScreen;
 import org.robovm.apple.uikit.UISplitViewController;
 import org.robovm.apple.uikit.UIView;
@@ -42,16 +52,41 @@ public class Main extends UIApplicationDelegateAdapter implements GLKViewControl
 		
 		viewController.setView(view);
 		
-		UINavigationController navi = new UINavigationController(viewController);
+		final Navigation navi = new Navigation(viewController);
 		
 		split.setViewControllers(new NSArray<UIViewController>(secontView, navi));
 		
-		viewController.getNavigationItem().setLeftBarButtonItem(split.getDisplayModeButtonItem());
+		final Menu menu = new Menu();
+		menu.setModalPresentationStyle(UIModalPresentationStyle.Popover);
+		menu.setPreferredContentSize(new CGSize(100, 100));
 		
-//		final MyGLKViewControllerDelegate gcDelegate = new MyGLKViewControllerDelegate(this);
+		navi.addChildViewController(menu);
+		
+		final UIButton button = new UIButton(UIButtonType.RoundedRect);
+		button.setFrame(new CGRect(0, 0, 100, 100));
+		button.setTitle("check", UIControlState.Normal);
+		button.getTitleLabel().setFont(UIFont.getBoldSystemFont(22));
+		button.addOnTouchUpInsideListener(new UIControl.OnTouchUpInsideListener() {
+			
+			@Override
+			public void onTouchUpInside(UIControl control, UIEvent event) {
+				navi.presentViewController(menu, true, null);
+				
+				UIPopoverPresentationController popover = menu.getPopoverPresentationController();
+				popover.setPermittedArrowDirections(UIPopoverArrowDirection.Left);
+				popover.setDelegate(navi);
+				popover.setBackgroundColor(UIColor.cyan());
+				popover.setSourceView(button);
+				popover.setSourceRect(new CGRect(0, 0, button.getFrame().getWidth(), button.getFrame().getHeight()));
+			}
+		});
+		navi.getNavigationBar().addSubview(button);
+		
+//		viewController.getNavigationItem().setLeftBarButtonItem(split.getDisplayModeButtonItem());
+		
 		viewController.setDelegate(this);
 		viewController.setPreferredFramesPerSecond(60);
-		this.window.setRootViewController(split);
+		this.window.setRootViewController(navi);
 		
 		this.window.setBackgroundColor(UIColor.white());
 		this.window.makeKeyAndVisible();
---
+++ src/main/java/org/mklab/mikity/ios/Menu.java
@@ -0,0 +1,32 @@
+package org.mklab.mikity.ios;
+
+import org.robovm.apple.uikit.UITableView;
+import org.robovm.apple.uikit.UITableViewCell;
+import org.robovm.apple.uikit.UITableViewCellAccessoryType;
+import org.robovm.apple.uikit.UITableViewCellSelectionStyle;
+import org.robovm.apple.uikit.UITableViewCellStyle;
+import org.robovm.apple.uikit.UITableViewController;
+
+public class Menu extends UITableViewController{
+	public Menu() {
+		UITableView view = getTableView();
+				
+		UITableViewCell cell = view.dequeueReusableCell("model");
+		
+		if (cell == null) {
+			cell = new UITableViewCell(
+					UITableViewCellStyle.Default, "model");
+			cell.setAccessoryType(UITableViewCellAccessoryType.DetailDisclosureButton);
+			
+			cell.setSelectionStyle(UITableViewCellSelectionStyle.Default);
+			cell.getTextLabel().setText("Model");
+			cell.setAccessoryType(UITableViewCellAccessoryType.DetailButton);
+			
+			cell.setEditing(true);
+			
+			view.addSubview(cell);
+			
+		}
+	}
+
+}
